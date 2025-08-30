cabf811d85d6d814c19ad213aafd312c84416a5c
abdullah-621 <abdullahmasum621@gmail.com>
2025-08-30 11:09:33 +0600

    modify

--- Vector/vec.cpp
+++ Vector/vec.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    
+    vector<int>arr = {1, 2, 3, 4, 5};
+
+    vector<int> brr;
+    brr.assign(arr.begin(), arr.end());
     return 0;
 }
