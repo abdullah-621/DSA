3528aa56546e9b957e1f435c3a540155098ad106
abdullah-621 <abdullahmasum621@gmail.com>
2025-10-07 09:51:24 +0600

    merge sort

--- Sorting/merge.cpp
+++ Sorting/merge.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void devide_sort(int arr[], int first, int last)
+void merge(int arr[], int start, int mid, int end)
 {
-  int mid = (first + last) / 2;
-  cout << arr[mid];
+  vector<int> temp(end - start + 1);
+  int left = start;
+  int right = mid + 1;
+  int index = 0;
+
+  while (left <= mid && right <= end)
+  {
+    if (arr[left] >= arr[right])
+    {
+      temp[index++] = arr[left++];
+    }
+    else
+    {
+      temp[index++] = arr[right++];
+    }
+  }
+
+  // if left array is remain any value
+  while(left <= mid)
+  {
+    temp[index++] = arr[left++];
+  }
+
+// if right array is remain any value
+  while(right <= end)
+  {
+    temp[index++] = arr[right++];
+  }
+
+  // put this value into original array
+  index = 0;
+  while (start <= end)
+  {
+    arr[start++] = temp[index++];
+  }
+  
+}
+
+void mergesort(int arr[], int start, int end)
+{
+  if (start == end)
+    return;
+  int mid = start + (end - start) / 2;
+
+  mergesort(arr, start, mid);
+  mergesort(arr, mid + 1, end);
+
+  merge(arr, start, mid, end);
 }
 
 int main()
@@ -12,7 +59,7 @@ int main()
   int n = 5;
   int arr[] = {5, 4, 3, 2, 1};
 
-  devide_sort(arr, 0, n);
+  mergesort(arr, 0, n - 1);
 
   for (int i : arr)
   {
